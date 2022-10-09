b2a55f9f83d88f1966cfce78b524eacd32107a06
jwatts777 <watts.jordan777@gmail.com>
2022-10-09 15:20:50 -0500

    Final Commit Before Submission

--- src/routes/MainPage/page.svelte
+++ src/routes/MainPage/page.svelte
@@ -1,1 +1,15 @@
-<h1>MainPage</h1>
+<div class='grid'>
+<div class='text-8xl mx-auto'>MainPage Example</div>
+<div class='text-8xl mx-auto'>2nd Title</div>
+<div class='grid-auto h-128 mx-auto'>
+    <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+</div>
+</div>
--- src/routes/NavBar/+page.svelte
+++ src/routes/NavBar/+page.svelte
@@ -1,16 +1,16 @@
 <script>
     let exampleTitle = "Example Title"
 
-    let exampleLinkButtonValue = "About"
+    let exampleLinkButtonValue = "Example About"
 </script>
 
 
-<div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+<div class="navbar mb-2 shadow-lg text-neutral-content rounded-box">
     <div class="flex-1 px-2 mx-2">
         <span class="text-lg font-bold">
-            <label for="my-drawer" class="btn btn-primary drawer-checkbox">
+            <label for="my-drawer" class="btn drawer-checkbox">
                 <div class="flex-none" />
-                Example
+                Example Button
             </label>
         </span>
         <div class="mx-auto center text-align-center flex-none">{exampleTitle}</div>
@@ -18,7 +18,7 @@
 
     <div class="flex-none px-2 mx-2 lg:flex">
         <div class="flex items-stretch">
-            <a href="./About" class="btn btn-primary">{exampleLinkButtonValue}</a>
+            <a href="./About" class="btn">{exampleLinkButtonValue}</a>
         </div>
     </div>
 </div>
